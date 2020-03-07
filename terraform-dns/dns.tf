8836024af3abe588c215f858e04ccd3e4c4bac6d
Rami Al-Ghanmi <rami@ramialghanmi.com>
2020-03-07 09:33:11 -0800

    Add DNS entry for argo workflow ui

--- terraform-dns/dns.tf
+++ terraform-dns/dns.tf
@@ -15,3 +15,11 @@ resource "cloudflare_record" "echo2" {
   type    = "A"
   ttl     = 600
 }
+
+resource "cloudflare_record" "argo" {
+  zone_id = data.cloudflare_zones.workflow_zone.zones[0].id
+  name    = "argo"
+  value   = var.ipv4_address
+  type    = "A"
+  ttl     = 600
+}
